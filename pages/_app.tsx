0eafcb94a3167166b2c446162cc5d3a16c3115ea
Senhung <alexwongsenhung@gmail.com>
2021-03-02 12:43:36 +0800

    Add tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import React from "react";
 import { RecoilRoot } from "recoil";
+import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
