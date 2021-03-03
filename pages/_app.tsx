a0c6de14037e5121022eb0aced66acd389cd120a
Senhung <alexwongsenhung@gmail.com>
2021-03-04 00:33:01 +0800

    Add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import Head from "next/head";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import { Footer } from "../components/Footer";
@@ -9,6 +10,9 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
       <RecoilRoot>
+        <Head>
+          <title>Image Pixelater</title>
+        </Head>
         <Component {...pageProps} />
         <Footer />
       </RecoilRoot>
